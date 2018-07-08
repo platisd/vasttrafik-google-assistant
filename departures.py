26e2934453ecd563b245e4d7a38c40ac6e0eb544
Dimitris Platis <platisd@users.noreply.github.com>
2018-07-08 20:04:04 +0200

    Adapt further for responding to Google Assistant

--- departures.py
+++ departures.py
@@ -15,9 +15,23 @@ import os
 from datetime import datetime
 from collections import defaultdict
 
-LINDHOLMEN_ID = "9021014004490000"
-TEKNIKGATAN_ID = "9021014006675000"
-stations_of_interest = {"Lindholmen": LINDHOLMEN_ID, "Teknikgatan": TEKNIKGATAN_ID}
+
+class StationOfInterest:
+    def __init__(self, station_name, station_id, tracks):
+        self.name = station_name
+        self.id = station_id
+        self.tracks = tracks
+
+
+class Trip:
+    def __init__(self, vehicle_type, line, destination, minutes_left):
+        self.vehicle_type = vehicle_type
+        self.line = line
+        self.destination = destination
+        self.minutes_left = str(int(minutes_left))
+
+    def __repr__(self):
+        return self.vehicle_type + " " + self.line + " towards " + self.destination + " departs in " + self.minutes_left + " minutes."
 
 
 def init_vasttrafik_api_keys(path_to_api_keys):
@@ -69,8 +83,8 @@ def get_time(host="pool.ntp.org"):
 
 def get_next_trips(vasttrafik, stations):
     """
-    Fetches the next bus or tram trips for the specified list of station IDs
-    and connection to the Vasttrafik servers.
+    Fetches the next bus or tram trips for the specified list of
+    StationOfInterest and connection to the Vasttrafik servers.
 
     Returns A list of bus/tram line numbers and minutes to depart for the next
             two trips of each bus or tram line.
@@ -78,38 +92,37 @@ def get_next_trips(vasttrafik, stations):
     # Get the current time and date from an NTP server as the host might not have an RTC
     (current_date, current_time) = get_time()
     trips = defaultdict(list)  # A dictionary of lists, holding a list of departures for each bus
-    for stationName, stationID in stations.items():
+    for station in stations:
         # Get the departures for each station we are interested in
         try:
-            departures = vasttrafik.get_departures(stationID, date=current_date, time=current_time)
+            departures = vasttrafik.get_departures(station.id, date=current_date, time=current_time)
         except Exception as e:
-            print("Connection failure for station %s" % stationName)
+            print("Connection failure for station %s" % station.name)
             departures = []  # If something went wrong, empty the departures list so we don't try to iterate through them
         for departure in departures:
-            if departure['track'] < 'C':  # We only care for buses going towards the center for now
+            if departure['track'] in station.tracks:
                 try:
                     # Sometimes rtTime is not returned, so fall back to normal time instead
-                    departureTime = departure['rtTime'] if 'rtTime' in departure else departure['time']
-                    trips[(departure['sname'], departure['direction'])].append(departureTime)
-                    # print ("Bus %s leaves towards %s at %s" % (departure['sname'], departure['direction'], departure['rtTime']))
+                    departure_time = departure['rtTime'] if 'rtTime' in departure else departure['time']
+                    trips[(departure['sname'], departure['direction'], departure['type'])].append(departure_time)
                 except Exception as e:
                     print("Error while parsing server response")
-    # print (sorted(trips.items()))
+
     nextTrips = []
-    for (busLine, destination), departureTimes in trips.items():
-            remainingDepartures = 2  # The number of departures that we care to show
-            for departureTime in departureTimes:
-                remainingDepartures -= 1
-                if remainingDepartures < 0:
+    for (line, destination, vehicle_type), departure_times in trips.items():
+            remaining_departures = 2  # The number of departures that we care to show
+            for departure_time in departure_times:
+                remaining_departures -= 1
+                if remaining_departures < 0:
                     break
-                minutesToLeave = ((datetime.strptime(departureTime, "%H:%M") - datetime.strptime(current_time, "%H:%M")).total_seconds() / 60)
+                minutes_to_leave = ((datetime.strptime(departure_time, "%H:%M") - datetime.strptime(current_time, "%H:%M")).total_seconds() / 60)
                 # If a bus leaves the next day, the above result will be a negative number, therefore we need to completement it
                 # with the amount of minutes in a day (1440)
-                if minutesToLeave < 0:  # meaning that the next departure time is on the next day
+                if minutes_to_leave < 0:  # meaning that the next departure time is on the next day
                     MINUTES_IN_DAY = 1440
-                    minutesToLeave += MINUTES_IN_DAY
-                # print ("Bus %s, leaves in %d" % (busLine, minutesToLeave))
-                nextTrips.append((busLine, destination, minutesToLeave))
+                    minutes_to_leave += MINUTES_IN_DAY
+                nextTrips.append(Trip(vehicle_type, line, destination, minutes_to_leave))
+
     return nextTrips
 
 
@@ -124,7 +137,3 @@ def init():
     (api_key, api_secret) = init_vasttrafik_api_keys("api-config")
 
     return init_connection(api_key, api_secret)
-
-
-if __name__ == "__main__":
-    init()
