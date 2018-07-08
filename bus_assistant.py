7710a7657a81f4304365e65fd708ef83d70c56e1
Dimitris Platis <platisd@users.noreply.github.com>
2018-07-08 21:21:02 +0200

    Call departures module through the bus assistant

--- bus_assistant.py
+++ bus_assistant.py
@@ -10,19 +10,32 @@ upcoming bus or tram departures and the Google Assistant service.
 
 import json
 from flask import Flask, request, make_response, jsonify
+import departures
 
 app = Flask("Personal-Bus-Stop")
+# The currently supported stations we are interested in. The station id's are
+# hardcoded and the id's can be discovered through the location API. E.g.:
+# https://api.vasttrafik.se/bin/rest.exe/v2/location.name?input=almedal&format=json
+home = departures.StationOfInterest("Lindholmen", "9021014004490000", ['A', 'B'])
+work = departures.StationOfInterest("Almedal", "9021014001050000", ['A'])
 
 
 def respond(fullfilment):
     return make_response(jsonify({'fulfillmentText': fullfilment}))
 
 
+def trips_to_response(trips):
+    response = ""
+    for trip in trips:
+        response += str(trip) + "\n"
+
+    return response
+
+
 @app.route('/departures', methods=['POST'])
 def webhook():
     try:
         req = request.get_json(silent=True, force=True)
-
         print(json.dumps(req))
 
         location_data = req.get('queryResult').get('parameters').get('location')
@@ -30,16 +43,18 @@ def webhook():
             return respond("I do not know where this is.")
 
         location = location_data.get('shortcut')
+        vasttrafik = departures.init()  # Establish connection to Vasttrafik
 
         if location == 'home':
-            res = "No place like home"
+            stations_of_interest = [home]
         elif location == 'work':
-            res = "Your work is cool"
+            stations_of_interest = [work]
         else:
-            res = "I am not sure where " + location + " is."
+            return respond("I am not sure where " + location + " is.")
 
-        return respond(res)
+        trips = departures.get_next_trips(vasttrafik, stations_of_interest)
 
+        return respond(trips_to_response(trips))
     except Exception as e:
         print(e)
         return respond("Sorry, an error occurred. Please check the server logs.")
